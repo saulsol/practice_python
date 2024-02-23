a274e664493c3a06125c04810ecc982ffcb7b1c5
saul <dlathf3210@naver.com>
2024-02-23 16:43:27 +0900

    02/23

--- test/반복문 연습.py
+++ test/반복문 연습.py
@@ -3,3 +3,21 @@ i = 0
 while i < 8:
     print("화이팅")
     i = i + 1;
+
+print("-----------------------")
+
+
+for i in range(0, 8):
+    print(i)
+
+
+print("-----------------------")
+
+fruits = ['apple', 'banana', 'pineapple', 'watermelon', 'pitch']
+
+for i in range(0, len(fruits)):
+    print(fruits[i])
+print("-----------------------")
+
+for fruit in fruits:
+    print(fruit)
